770243ff0ce79fe95fc31d3e2e92506c98f1964b
Konstantin <koc.voytenko@gmail.com>
2021-11-27 08:15:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 252268;
+        int mile = 20;
+
+        int bonus = ticket / mile;
+
+        System.out.println(bonus);
+    }
+}
